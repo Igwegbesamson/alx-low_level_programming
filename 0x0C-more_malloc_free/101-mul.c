51a99187d0bc1a815cd466a37f27d13333efc156
Igwegbesamson <35536836+Igwegbesamson@users.noreply.github.com>
2022-06-24 09:03:08 +0100

    Add together

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,107 +1,56 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
-
 /**
- * is_digit - checks for digit
- *
- * @s: string operand
- *
- * Return: 0 or 1
+ * _atoi - Converts a character array to an integer
+ * @s: character array to convert
+ * Return: returns integer from array, returns 0 if none found
  */
-
-int is_digit(char *s)
+int _atoi(char *s)
 {
-	int i = 0;
-
-	while (s[i])
-	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-/**
- * _strlen - string length
- *
- * @s: string operand
- *
- * Return: i
- */
-
-int _strlen(char *s)
+int retval, i, neg;
+i = 0;
+retval = 0;
+neg = -1;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 {
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
+if (s[i] == '-')
+neg *= -1;
+i++;
 }
-
-/**
- * errors - handles errors for main
- */
-
-void errors(void)
-{
-	printf("Error\n");
-	exit(98);
+while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+retval = (retval * 10) - (s[i++] - '0');
+return (retval *neg);
 }
-
 /**
  * main - multiplies two positive numbers
- *
- * @argc: argument counter
- *
- * @argv: argument vector
- *
- * Return: always 0 (Success)
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
-
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int l1, l2, l, i, n, digit1, digit2, *ptr, a = 0;
-
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	l1 = _strlen(s1);
-	l2 = _strlen(s2);
-	l = l1 + l2 + 1;
-	ptr = malloc(sizeof(int) * l);
-	if (!ptr)
-		return (1);
-	for (i = 0; i <= l1 + l2; i++)
-		ptr[i] = 0;
-	for (l1 = l1 - 1; l1 >= 0; l1--)
-	{
-		digit1 = s1[l1] - '0';
-		n = 0;
-		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
-		{
-			digit2 = s2[l2] - '0';
-			n += ptr[l1 + l2 + 1] + (digit1 * digit2);
-			ptr[l1 + l2 + 1] = n % 10;
-			n /= 10;
-		}
-		if (n > 0)
-			ptr[l1 + l2 + 1] += n;
-	}
-	for (i = 0; i < l - 1; i++)
-	{
-		if (ptr[i])
-			a = 1;
-		if (a)
-			_putchar(ptr[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(ptr);
-	return (0);
+int a, b, i, j;
+unsigned long mul;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
+{
+printf("Error\n");
+exit(98);
+}
+}
+}
+a = _atoi(argv[1]);
+b = _atoi(argv[2]);
+mul = a *b;
+printf("%lu\n", mul);
+return (0);
 }
